29f5101638848304b0d576206c5d6839d20524bc
Mattias Lasersköld <mattias.l.sk@gmail.com>
2020-03-23 21:36:26 +0100

    Updated example

--- example1.cpp
+++ example1.cpp
@@ -1,59 +1,56 @@
 #include "unittest.h"
 
-//This struct would normaly be written in another file
-//and the test case would only linked to that file for testing
+// This struct would normaly be written in another file
+// and the test case would only linked to that file for testing
 
-struct TestStruct{
-	TestStruct(){}	
-	
-	TestStruct(int value){
-		a(value);
-	}	
-	
-	bool operator== (TestStruct& s){
-		if (a() == s.a()){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
-	int a() {
-		return _a;
-	}
-	
-	void a(int val){
-		_a = val;
-	}
-	
-	int _a;
+struct TestStruct {
+    TestStruct() {}
+
+    TestStruct(int value) {
+        a(value);
+    }
+
+    bool operator==(TestStruct &s) {
+        if (a() == s.a()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    int a() {
+        return _a;
+    }
+
+    void a(int val) {
+        _a = val;
+    }
+
+    int _a = 0;
 };
 
 
-//Begining of actual test code
-//This is what is supposed to be in a test file
+// Begining of actual test code
+// This is what is supposed to be in a test file
 
 TEST_SUIT_BEGIN
 
-TEST_CASE("equalization test"){
-	TestStruct testStruct1(1), testStruct2(1);
-	
-	ASSERT(testStruct1 == testStruct2, "something went wrong");
-	
-	//The same test but in another way
-	ASSERT_EQ(testStruct1.a(), testStruct2.a());
-	
-	return 0;
+TEST_CASE("equalization test") {
+    TestStruct testStruct1(1), testStruct2(1);
+
+    ASSERT(testStruct1 == testStruct2, "something went wrong");
+
+    // The same test but in another way
+    ASSERT_EQ(testStruct1.a(), testStruct2.a());
 }
 
-TEST_CASE("failing test"){
-	TestStruct testStruct1, testStruct2;
-	testStruct1.a(1);
-	testStruct2.a(2);
-	
-	ASSERT_EQ(testStruct1.a(), testStruct2.a());
-	return 0;
+TEST_CASE("failing test") {
+    TestStruct testStruct1, testStruct2;
+    testStruct1.a(1);
+    testStruct2.a(2);
+
+    ASSERT_EQ(testStruct1.a(), testStruct2.a());
 }
 
 TEST_SUIT_END
